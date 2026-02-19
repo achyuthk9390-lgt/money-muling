0b87593c3e88f8af2b34c50ff4b3030fbf59d92e
achyuthk9390-lgt <achyuthk9390@gmail.com>
2026-02-19 15:11:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,30 @@
-suspicious_accounts = {}
+@app.route("/upload", methods=["POST"])
+def upload():
+    file = request.files["file"]
+    df = pd.read_csv(file)
 
-cycles = list(nx.simple_cycles(G))
-ring_count = 1
+    G = nx.DiGraph()
 
-for cycle in cycles:
-    if 3 <= len(cycle) <= 5:
-        rings.append({
-            "ring_id": f"RING_{ring_count}",
-            "members": cycle
-        })
-        for node in cycle:
-            suspicious_accounts[node] = suspicious_accounts.get(node, 0) + 90
-        ring_count += 1
+    for _, row in df.iterrows():
+        G.add_edge(row["sender_id"], row["receiver_id"])
 
-# Fan-in detection
-for node in G.nodes():
-    if G.in_degree(node) >= 5:
-        suspicious_accounts[node] = suspicious_accounts.get(node, 0) + 70
+    suspicious_accounts = {}
+    rings = []
 
-# Cap score at 100
-for node in suspicious_accounts:
-    if suspicious_accounts[node] > 100:
-        suspicious_accounts[node] = 100
-return render_template("results.html",
-                       suspicious=suspicious_accounts,
-                       rings=rings)
+    cycles = list(nx.simple_cycles(G))
+    ring_count = 1
+
+    for cycle in cycles:
+        if 3 <= len(cycle) <= 5:
+            rings.append({
+                "ring_id": f"RING_{ring_count}",
+                "members": cycle
+            })
+            for node in cycle:
+                suspicious_accounts[node] = suspicious_accounts.get(node, 0) + 90
+            ring_count += 1
+
+    return render_template("results.html",
+                           suspicious=suspicious_accounts,
+                           rings=rings)
 
